78b254798ccb4a70eeec40791c236e8bbf566b1b
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-05-27 16:06:03 +0300

    Update toggle packing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import { useState } from 'react';
 
-// const initialItems = [
-//   { id: 1, description: 'Passports', quantity: 2, packed: false },
-//   { id: 2, description: 'Socks', quantity: 12, packed: false },
-//   { id: 3, description: 'Charger', quantity: 1, packed: true },
-// ];
-
 export default function App() {
   const [items, setItems] = useState([]);
+
   function handleAddItems(item) {
     setItems(items => [...items, item]);
   }
   function handleDeleteItem(id) {
     setItems(items => items.filter(item => item.id !== id));
   }
+  function handleToggleItem(id) {
+    setItems(items =>
+      items.map(item =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -64,20 +70,30 @@ function Form({ onAddItems }) {
     </form>
   );
 }
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map(item => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            key={item.id}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
